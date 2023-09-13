ecfd4dab69e1ff199d0c14d24c3994657dd9b1c5
3kezoh <3kezoh@gmail.com>
2023-09-13 23:33:28 +0200

    fix: fix typo

--- index.ts
+++ index.ts
@@ -7,14 +7,10 @@ import { parse } from "./src/parse";
 async function main() {
   const treasmureMapPath = path.join(__dirname, "treasure-map.txt");
   const treasureMap = await read(treasmureMapPath);
-  const parsedTreasmureMap = parse(treasureMap);
-  const simulated = simulate(parsedTreasmureMap);
+  const parsedTreasureMap = parse(treasureMap);
+  const simulated = simulate(parsedTreasureMap);
 
-  await write(
-    path.join(__dirname, "result.txt"),
-    parsedTreasmureMap,
-    simulated
-  );
+  await write(path.join(__dirname, "result.txt"), parsedTreasureMap, simulated);
 
   console.log(simulated);
 }
